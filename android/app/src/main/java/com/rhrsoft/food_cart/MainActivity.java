c51784e274213669155d52bd46d6309f0809c93e
Ratul Hasan Ruhan <ratulhasan1644@gmail.com>
2024-06-29 22:20:39 +0600

    Initial commit

---
+++ android/app/src/main/java/com/rhrsoft/food_cart/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rhrsoft.food_cart;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const primaryColor = Color(0xFFFF6B23);
+const accentColor = Color(0xFF02308E);
+const textColor = Color(0xFF010832);
+
+const backgroundColor = Color(0xFFFFF1E9);
